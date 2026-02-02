160fc3b8fd59e47eaf42fea83098ef549fbf8565
Mahmoud Elgendi <Mahmoud.E@miviewis.com>
2026-02-02 16:00:22 -0600

    local hosting to docker engine and cloud sql server

---
+++ src/School.Api/Data/StudentSeeder.cs
@@ -0,0 +1,46 @@
+﻿using School.Api.Models;
+using System;
+
+namespace School.Api.Data
+{
+    public static class StudentSeeder
+    {
+        public static async Task SeedAsync(ApplicationDbContext context)
+        {
+            if (context.Students.Any())
+                return; // idempotent (runs only once)
+
+            var students = new List<Student>
+        {
+            new Student
+            {
+                Id = Guid.NewGuid(),
+                FirstName = "John",
+                LastName = "Doe",
+                Email = "john.doe@test.com",
+                DateOfBirth = new DateTime(1998, 5, 12)
+            },
+            new Student
+            {
+                Id = Guid.NewGuid(),
+                FirstName = "Jane",
+                LastName = "Smith",
+                Email = "jane.smith@test.com",
+                DateOfBirth = new DateTime(2000, 1, 25)
+            },
+            new Student
+            {
+                Id = Guid.NewGuid(),
+                FirstName = "Mahmoud",
+                LastName = "Elgendi",
+                Email = "mahmoud@test.com",
+                DateOfBirth = new DateTime(1995, 3, 10)
+            }
+        };
+
+            context.Students.AddRange(students);
+            await context.SaveChangesAsync();
+        }
+    }
+
+}
--- src/School.Api/Program.cs
+++ src/School.Api/Program.cs
@@ -28,3 +28,11 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    await db.Database.MigrateAsync();
+    await StudentSeeder.SeedAsync(db);
+}
+
